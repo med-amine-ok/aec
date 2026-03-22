fc2674fd0634256da9c582950cf6d83dd23af84d
OULD KHAOUA  mohamed amine <ouldkhaoua.pro@gmail.com>
2026-03-22 16:53:43 +0100

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -15,7 +15,7 @@ export default function Register() {
         <div className="relative z-10">
           <Nav />
           <Regclose/>
-          {/* <Reg/> */}
+          <Reg/> 
         </div>
       </div>
     </ErrorBoundary>
